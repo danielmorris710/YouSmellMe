c9a164f510b35f027b552ddad7455c3c2ae2e1f7
Daniel Morris <uptownmelo#gmail.com>
2022-08-04 18:22:04 -0400

    Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express')
 require('dotenv').config()
+const fragrance = require('./models/fragrance')
 const app = express()
 const port = process.env.PORT || 3003
 
+//my routes
 app.get('/', (req, res)=>{
     res.send('Welcome to the You Smell Me!')
 })
 
-app.listen(port,() => {
-    console.log('I am listening on port' , port);
+app.get('/fragrance', (req,res) =>{
+    res.send(fragrance)
+})
+
+//port 
+app.listen(port, () => {
+    console.log("I am listening on port", port);
 })
